1d00d2c58fb5cd3585e321e3eea54fc8ae0178d8
Luis Tomas Bolivar <ltomasbo@redhat.com>
2023-06-14 16:50:39 +0200

    Retry get_ovs_patch_port_ofport if empty port
    It seems sometimes we are getting [] instead of the ofport,
    possibly due to a race between creating the interface and
    attaching it to the ovs bridge. This ensure it is also
    retried in that case
    
    Change-Id: I2f0766c3a018336168c02ab77bbdfc9ed246a150

--- ovn_bgp_agent/drivers/openstack/utils/wire.py
+++ ovn_bgp_agent/drivers/openstack/utils/wire.py
@@ -16,7 +16,6 @@ import pyroute2
 
 from oslo_config import cfg
 from oslo_log import log as logging
-import tenacity
 
 from ovn_bgp_agent import constants
 from ovn_bgp_agent.drivers.openstack.utils import ovs
@@ -139,16 +138,8 @@ def unwire_provider_port(routing_tables_routes, port_ips, bridge_device,
         return True
 
 
-@tenacity.retry(
-    retry=tenacity.retry_if_exception_type(agent_exc.PatchPortNotFound),
-    wait=tenacity.wait_fixed(1),
-    stop=tenacity.stop_after_delay(5),
-    reraise=True)
 def _ensure_updated_mac_tweak_flows(localnet, bridge_device, ovs_flows):
-    try:
-        ofport = ovs.get_ovs_patch_port_ofport(localnet)
-    except Exception:
-        raise agent_exc.PatchPortNotFound(localnet=localnet)
+    ofport = ovs.get_ovs_patch_port_ofport(localnet)
     if ofport not in ovs_flows[bridge_device]['in_port']:
         ovs_flows[bridge_device]['in_port'].append(ofport)
         ovs.ensure_mac_tweak_flows(bridge_device,
