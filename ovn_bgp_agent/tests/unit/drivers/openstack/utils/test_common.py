acb53b40099b3a1c3d730ebbd898b0febe52070d
Jakub Libosvar <libosvar@redhat.com>
2024-08-01 16:01:29 +0000

    Remove duplicated _get_network method
    The method was tied to an instance of some events but didn't actually
    modify state of the instance. It was replaced with a generic helper
    function.
    
    Change-Id: I0d24ead8e44cde1df62df77408801cbbc758c4a6
    Signed-off-by: Jakub Libosvar <libosvar@redhat.com>

---
+++ ovn_bgp_agent/tests/unit/drivers/openstack/utils/test_common.py
@@ -0,0 +1,38 @@
+# Copyright 2024 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from ovn_bgp_agent.drivers.openstack.utils import common
+from ovn_bgp_agent.tests import base as test_base
+from ovn_bgp_agent.tests import utils as test_utils
+
+
+class TestGetFromExternalIds(test_base.TestCase):
+    def test_all_present(self):
+        key = 'foo'
+        value = 'bar'
+        row = test_utils.create_row(external_ids={key: value})
+
+        observed_value = common.get_from_external_ids(row, key)
+        self.assertEqual(value, observed_value)
+
+    def test_external_ids_missing(self):
+        row = test_utils.create_row()
+
+        self.assertIsNone(common.get_from_external_ids(row, 'key'))
+
+    def test_key_missing(self):
+        row = test_utils.create_row(external_ids={})
+
+        self.assertIsNone(common.get_from_external_ids(row, 'key'))
