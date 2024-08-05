d142f397027ad602145f9173d3a219958a9f7512
Jakub Libosvar <libosvar@redhat.com>
2024-08-05 18:32:02 +0000

    Move methods out of LSP events
    The methods are moved to a function to ports utility module as the
    methods were not reading or modifying the instance state.
    
    Change-Id: Iae9d1d1f2f5d5bab38c290a5d6e8f5cf8136e108
    Signed-off-by: Jakub Libosvar <libosvar@redhat.com>

--- ovn_bgp_agent/tests/unit/drivers/openstack/utils/test_port.py
+++ ovn_bgp_agent/tests/unit/drivers/openstack/utils/test_port.py
@@ -13,8 +13,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from ovn_bgp_agent import constants
 from ovn_bgp_agent.drivers.openstack.utils import port
 from ovn_bgp_agent.tests import base as test_base
+from ovn_bgp_agent.tests import utils as test_utils
 
 
 class TestHasIpAddressDefined(test_base.TestCase):
@@ -35,3 +37,48 @@ class TestHasIpAddressDefined(test_base.TestCase):
         self.assertTrue(
             port.has_ip_address_defined(
                 'aa:bb:cc:dd:ee:ff 10.10.1.16 10.10.1.17 10.10.1.18'))
+
+
+class TestGetFip(test_base.TestCase):
+    def test_get_fip(self):
+        value = 'foo'
+        lsp = test_utils.create_row(
+            external_ids={constants.OVN_FIP_EXT_ID_KEY: value})
+        observed = port.get_fip(lsp)
+
+        self.assertEqual(value, observed)
+
+    def test_get_fip_not_present(self):
+        lsp = test_utils.create_row()
+        self.assertIsNone(port.get_fip(lsp))
+
+
+class TestHasAdditionalBinding(test_base.TestCase):
+    def test_has_multiple_chassis(self):
+        lsp = test_utils.create_row(
+            options={constants.OVN_REQUESTED_CHASSIS: "chassis1,chassis2"})
+
+        self.assertTrue(port.has_additional_binding(lsp))
+
+    def test_has_one_chassis(self):
+        lsp = test_utils.create_row(
+            options={constants.OVN_REQUESTED_CHASSIS: "chassis1"})
+
+        self.assertFalse(port.has_additional_binding(lsp))
+
+    def test_no_options(self):
+        lsp = test_utils.create_row()
+
+        self.assertFalse(port.has_additional_binding(lsp))
+
+    def test_no_requested_chassis(self):
+        lsp = test_utils.create_row(
+            options={})
+
+        self.assertFalse(port.has_additional_binding(lsp))
+
+    def test_empty_requested_chassis(self):
+        lsp = test_utils.create_row(
+            options={constants.OVN_REQUESTED_CHASSIS: ""})
+
+        self.assertFalse(port.has_additional_binding(lsp))
