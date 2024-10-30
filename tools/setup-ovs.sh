92d3ead8bd63e8e28e6a019101a2b7d6a4151f5f
Jakub Libosvar <libosvar@redhat.com>
2024-10-30 21:52:25 +0000

    functional: Introduce a base class for OVN func testing
    The patch creates a base classes that inherit from the ovsdbapp project.
    A new API fixture is added to be able to use OVN BGP agent OVN IDL in
    the functional tests.
    
    Change-Id: I139d6798a1ed2f51d447c86f16dad2aa3892412b
    Signed-off-by: Jakub Libosvar <libosvar@redhat.com>
    (cherry picked from commit a01dddcefa81044da198ada6f98504f65e24517a)

---
+++ tools/setup-ovs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+# This is a copy from the ovsdbapp projects
+
+OVN_BRANCH=${OVN_BRANCH:-main}
+
+if [ "$OVN_SRCDIR" -a ! -d "$OVN_SRCDIR" ]; then
+    echo "Building OVN branch $OVN_BRANCH in $OVN_SRCDIR"
+    mkdir -p $OVN_SRCDIR
+    git clone --recurse-submodules https://github.com/ovn-org/ovn.git $OVN_SRCDIR
+    pushd $OVN_SRCDIR
+    git checkout $OVN_BRANCH
+    pushd ovs
+    ./boot.sh && PYTHON=/usr/bin/python ./configure && make -j$(($(nproc) + 1))
+    popd
+    ./boot.sh && PYTHON=/usr/bin/python ./configure && make -j$(($(nproc) + 1))
+    popd
+fi
